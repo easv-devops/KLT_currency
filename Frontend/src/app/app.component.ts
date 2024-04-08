cb1610f4bf5935364818f295981d397d96674412
Tomas1974 <114471994+Tomas1974@users.noreply.github.com>
2024-04-08 11:57:22 +0200

    Etablering af angular
    Etablering af angular frontend

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+
+  <ion-content>
+    <ion-row>
+      
+
+
+
+
+    </ion-row>
+
+
+
+
+
+
+
+
+
+
+  </ion-content>
+
+
+
+
+
+  `,
+
+})
+export class AppComponent {
+  constructor() {}
+}
