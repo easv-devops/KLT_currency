2af3c783e618204d375cb3a3fa46093b70a31abd
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2024-04-12 11:26:44 +0200

    Update to comply with sonarqube
    removed plaintext pw from utilities.

--- infrastructure/Utilities.cs
+++ infrastructure/Utilities.cs
@@ -3,6 +3,6 @@ namespace infrastructure;
 public class Utilities
 {
    private static readonly string DBPassword = new(Environment.GetEnvironmentVariable("PGPASSWORD"));
-   public static readonly string connectionStringDev = "Server=localhost:5432;Database=postgres;User Id=postgres;Password=" + "secret" + ";";
-   public static readonly string connectionStringProd = "Server=app-database:5432;Database=postgres;User Id=postgres;Password=" + "secret" + ";";
+   public static readonly string connectionStringDev = "Server=localhost:5432;Database=postgres;User Id=postgres;Password=" + DBPassword + ";";
+   public static readonly string connectionStringProd = "Server=app-database:5432;Database=postgres;User Id=postgres;Password=" + DBPassword + ";";
 }
